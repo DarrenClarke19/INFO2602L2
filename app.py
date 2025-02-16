876be81aacdfaa81bf02ebe9e65f48d571d78e44
Darren <darrenjustinclarke@yahoo.com>
2025-02-16 17:53:30 -0400

    final push

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from models import db  
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.app_context().push()
+
+
+db.init_app(app)
